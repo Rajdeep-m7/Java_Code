668dc54fa34da5b8ea57623936ce4cc4e50d7656
rajdeep Majumdar <amazumder781@gmail.com>
2026-01-12 20:38:38 +0530

    reverse sublist in LL

--- LinkedList/ReverseLL.java
+++ LinkedList/ReverseLL.java
@@ -51,6 +51,30 @@ public class ReverseLL {
 
     head = prev;
 }
+    public void reverseBetween(int left, int right) {
+    if (head == null || left == right) return;
+
+    Node dummy = new Node(0);
+    dummy.next = head;
+    Node prev = dummy;
+
+    for (int i = 1; i < left; i++) {
+        prev = prev.next;
+    }
+
+    Node current = prev.next;
+    Node next = null;
+
+    for (int i = 0; i < right - left; i++) {
+        next = current.next;
+        current.next = next.next;
+        next.next = prev.next;
+        prev.next = next;
+    }
+
+    head = dummy.next;
+}
+
 
 
     public static void main(String[] args) {
@@ -59,8 +83,9 @@ public class ReverseLL {
 
         list.insert(10);
         list.insert(20);
-        list.insert(20);
         list.insert(30);
+        list.insert(40);
+        list.insert(50);
 
         System.out.println("Linked List:");
         list.display();
@@ -68,5 +93,10 @@ public class ReverseLL {
         list.reverse();
         System.out.println("After reverse:");
         list.display();
+
+        list.reverseBetween(2, 4);
+        System.out.println("After reverse sublist:");
+        list.display();
+
     }
 }
