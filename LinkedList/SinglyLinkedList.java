13aa921f607b0750624cdf30d931a1ee48f97dcf
rajdeep Majumdar <amazumder781@gmail.com>
2026-01-16 08:49:33 +0530

    rotate LL

--- LinkedList/SinglyLinkedList.java
+++ LinkedList/SinglyLinkedList.java
@@ -59,6 +59,23 @@ public class SinglyLinkedList {
         prev.next = temp.next;
     }
 
+    public void rotateLL(int k){
+        if(head == null || head.next == null){
+            return;
+        }
+        for(int i =0 ; i<k ;i++){
+            Node temp = head;
+            Node prev= null;
+            while(temp.next != null){
+                prev= temp;
+                temp = temp.next;
+            }
+            temp.next= head;
+            head = temp;
+            prev.next= null;
+        }
+    }
+
     // Display the list
     public void display() {
         Node temp = head;
@@ -89,15 +106,17 @@ public class SinglyLinkedList {
         list.insert(20);
         list.insert(30);
         list.insert(30);
-        list.insertAtBeginning(5);
+        //list.insertAtBeginning(5);
 
         System.out.println("Linked List:");
         list.display();
 
         //list.delete(20);
         //System.out.println("After Deletion:");
-        list.duplicate();
-        System.out.println("After duplicate Deletion:");
+        //list.duplicate();
+        list.rotateLL(1);
+        //System.out.println("After duplicate Deletion:");
+        System.out.println("After rotate:");
         list.display();
     }
 }
